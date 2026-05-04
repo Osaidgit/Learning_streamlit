6e1e0c22bea7d82ec483bcf378fd95a41490c553
Osaidgit <alisoadi908@gmail.com>
2026-05-04 20:52:51 +0500

    02

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from rembg import remove
 from PIL import Image
 import io
+import streamlit as st
+
+st.title("Background Removal App")
+st.write("Upload an image to remove its background.")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    # Read the uploaded image
+    input_image = uploaded_file.read()
+    
+    # Process the image to remove the background
+    output_image = remove(input_image)
+    
+    # Display the result
+    st.image(output_image, caption='Processed Image', use_column_width=True)
+    st.button("Upload Image")
 
 def remove_background(input_path, output_path):
     # 1. Open the image
